04858d408d50f01c2d488ad78b5f608d17a1b153
bala-amypo <bala@amypo.in>
2025-12-17 04:10:07 +0000

    User commit

--- src/main/java/com/example/demo/config/basics.java
+++ src/main/java/com/example/demo/config/basics.java
@@ -1,4 +1,9 @@
 package com.example.demo.Controller;
 import org.springframework.web.bimd.annotation.*;
 @RestController
-
+public class basics{
+    @GetMappping("/home")
+    public String name(){
+        return "hello world"
+    }
+}
