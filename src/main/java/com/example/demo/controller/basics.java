b91a385f1375a66ae6701e026d507e567ca31f32
bala-amypo <bala@amypo.in>
2025-12-17 09:53:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/basics.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Controller;
-import org.springframework.web.bind.annotation.*;
-@RestController
-public class basics{
-    @GetMapping("/home")
-    public String name(){
-        return "hello world";
-    }
-}
